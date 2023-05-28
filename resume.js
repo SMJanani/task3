92841a7e759370bb8bbad57dcd20a2588826a81f
SMJanani <130118441+SMJanani@users.noreply.github.com>
2023-05-28 19:23:45 +0530

    Resume data in JSON format

---
+++ resume.js
@@ -0,0 +1,103 @@
+let myResume={
+    "basics": {
+      "name": "JANANI SM",
+      "email": "smjanani2504@gmail.com",
+      "phone": 9150017500,
+      "degree": "M.C.A.",
+      "dob": "2000-11-03",
+      "location": {
+        "address": "308, Amman Nagar, Sirupuluvapatti",
+        "postalCode": 641652,
+        "city": "Tirupur",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+            "network": "linkedin",
+            "username": "Janani SM",
+            "url": "https://www.linkedin.com/in/janani-sm-54196025b/"
+        },
+        {
+            "network": "github",
+            "username": "SMJanani",
+            "url": "https://github.com/SMJanani"
+        }
+      ]
+    },
+    "education": [
+      {
+        "institution": "Kongu Arts and Science College",
+        "course": "M.C.A.",
+        "department": "Computer Science",
+        "studyLevel": "Masters",
+        "studyType": "fulltime",
+        "batch start year": 2021,
+        "batch end year": 2023,
+        "gpa": 9.2,
+      },
+      {
+        "institution": "A.V.P. College of Arts and Science",
+        "course": "B.Sc.(C.S)",
+        "department": "Computer Science",
+        "studyLevel": "Bachelors",
+        "studyType": "fulltime",
+        "batch start year": 2018,
+        "batch end year": 2021,
+        "gpa": 8.9,
+      }
+    ],
+    "skills": [
+        {
+          "name": "Web Development",
+          "level": "Intermediate",
+          "keywords": [
+            "HTML",
+            "CSS",
+            "Javascript"
+          ]
+        },
+        {
+          "name": "Programming",
+          "level": "Intermediate",
+          "keywords": [
+            "C",
+            "C++",
+            "Java"
+          ]
+        }
+      ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+    "interests": [
+      {
+        "name": "Gardening, Programming"
+      }
+    ],
+    "projects": [
+        {
+          "name": "A Semantic Based Friend Recommendation System",
+          "description": "This project is developed for establishing friendship structure between forum user and facebook user based upon Friend Relationship based User Identification [FRUI] algorithm.",
+          "keywords": [
+            "ASP.NET", "FaceBook", "Forum"
+          ],
+          "startDate": "2021-01-14",
+          "endDate": "2021-03-24",
+          "type": "application"
+        },
+        {
+            "name": "Learning Management System",
+            "description": "This project aims to develop a hybrid application with a single codebase using Ionic with angular as frontend framework and Django as backend framework.",
+            "keywords": [
+              "Ionic", "Hybrid", "Django"
+            ],
+            "startDate": "2023-01-18",
+            "endDate": "2023-04-26",
+            "type": "hybrid application"
+          }
+      ],
+  }
+  console.log(myResume);
